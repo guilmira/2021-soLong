61b2a74909e229f460db0d33204b74f7b30c945d
Guillermo Mira Osuna <miraosuna@usal.es>
2021-10-07 09:39:25 +0200

    1

--- 1hooks_window.c
+++ 1hooks_window.c
@@ -6,7 +6,7 @@
 /*   By: guilmira <guilmira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 10:03:52 by guilmira          #+#    #+#             */
-/*   Updated: 2021/10/07 09:34:13 by guilmira         ###   ########.fr       */
+/*   Updated: 2021/10/07 09:39:09 by guilmira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,21 @@
 //meter los free en los exit. tambien los close window y destryo images.
 
 /** PURPOSE : closes program with output signal. */
-static int	ft_exit(int output)
+static int	ft_exit(t_program *game)
 {
-	exit(output);
+	clean_memory(game);
+	exit(0);
 }
 
 /** PURPOSE : closes window when ESC. */
-static int	ft_esc_exit(int keycode, int i)
+static int	ft_esc_exit(int keycode, t_program *game)
 {
 	if (keycode == 53)
-		exit(i);
-	return (i);
+	{
+		clean_memory(game);
+		exit(0);
+	}
+	return (0);
 }
 
 /** PURPOSE : open window.
@@ -37,8 +41,8 @@ void	init_window(t_program *game, t_vector window_dimensions)
 	window_dimensions.x, window_dimensions.y, WINDOW_NAME);
 	if (!game->window) //might need to free window later on. check manual..
 		full_shutdown(game, EX3);
-	mlx_key_hook(game->window, ft_esc_exit, 0);
-	mlx_hook(game->window, 17, 0, ft_exit, 0);
+	mlx_key_hook(game->window, ft_esc_exit, game);
+	mlx_hook(game->window, 17, 0, ft_exit, game);
 }
 
 
